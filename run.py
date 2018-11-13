973f85a87d5a10febfa0c322d5f6394d4d30a7cd
Rohit Nandwani <rohit@Rohits-MacBook-Pro.local>
2018-11-13 13:52:20 -0500

    added wordFrequnciesByTime computations

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+import pprint
 import collections
 import nltk
 
+
 from datetime import date, datetime, timedelta
+from dateutil import parser
 
 from helpers.fetch import twitterWrap
 from helpers.filter import keywords
@@ -13,6 +16,7 @@ for status in statuses:
     status.tokenized_tweet = nltk.word_tokenize(status.text)
     status.tagged_tweet = nltk.pos_tag(status.tokenized_tweet)
     status.filteredKeywords = keywords.filterKeywords(status.tagged_tweet)
+    status.created_at = parser.parse(status.created_at).replace(tzinfo=None)
     
 
 endTime = datetime.now()
@@ -21,10 +25,21 @@ intervalType = 'weeks'
 intervalValue = 2
 
 timePeriods = frequencies.createTimePeriods(startTime, endTime, intervalType, intervalValue)
-frequencies = {}
-
+wordFrequenciesByTime = collections.OrderedDict() 
+for timePeriod in timePeriods:
+    wordFrequenciesByTime[timePeriod] = []
 
 for status in statuses:
+    for timePeriod in reversed(timePeriods):
+        if status.created_at > timePeriod:
+            wordFrequenciesByTime[timePeriod].extend(status.filteredKeywords)
+            break
+
+#pprint.PrettyPrinter(indent=4).pprint(wordFrequenciesByTime)
+
+for key, value in wordFrequenciesByTime.items() :
+    print (value)
+
 
 
 #frequency_array = []
